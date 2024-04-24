df90be7cfe6544aa7d5b38244286ffb0dd3b57c2
amarjul <145123242+amarjul@users.noreply.github.com>
2024-04-24 14:39:13 +0200

    servus

---
+++ src/Kante.java
@@ -0,0 +1,11 @@
+public class Kante {
+    private Vertex _v1;
+    private Vertex _v2;
+    private int _weight;
+
+    public Kante(Vertex v1, Vertex v2, int weight) {
+        _v1 = v1;
+        _v2 = v2;
+        _weight = weight;
+    }
+}

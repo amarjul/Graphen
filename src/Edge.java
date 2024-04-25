dc63a7669a9db76f963224b1efa2589331c72527
amarjul <145123242+amarjul@users.noreply.github.com>
2024-04-25 09:01:02 +0200

    servus

--- src/Edge.java
+++ src/Edge.java
@@ -1,9 +1,9 @@
-public class Kante {
+public class Edge {
     private Vertex _v1;
     private Vertex _v2;
     private int _weight;
 
-    public Kante(Vertex v1, Vertex v2, int weight) {
+    public Edge(Vertex v1, Vertex v2, int weight) {
         _v1 = v1;
         _v2 = v2;
         _weight = weight;
